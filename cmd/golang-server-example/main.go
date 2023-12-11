46dc3ea5f2be6ce530d732c57f2859597f4bfd51
Adam Griswold <adam.griswold@nucor.com>
2023-12-11 21:24:16 +0000

    feat (main): Setup and run simple server
    Server runs on port 4221 and listens for specfic requests

--- cmd/golang-server-example/main.go
+++ cmd/golang-server-example/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+
+	"net"
+	"sync"
+
+	"github.com/acgriswold/golang-server-example/internal/handler"
+	"github.com/acgriswold/golang-server-example/internal/simpleRoutes"
 )
 
 func main() {
-	fmt.Println("Hello from golang")
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	handler.Check(err, "Failed to bind to port 4221", false)
+
+	var directoryFlag = flag.String("directory", ".", "directory to serve files from")
+	flag.Parse()
+
+	var wg sync.WaitGroup
+	for {
+		conn, err := l.Accept()
+		handler.Check(err, "Error accepting connection", true)
+
+		wg.Add(1)
+		go simpleRoutes.HandleConnection(conn, &wg, *directoryFlag)
+	}
+
+	wg.Wait()
 }
